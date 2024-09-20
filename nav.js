856a211659c06beecefd49feff3300adf4ca307d
zeyad mohammed nagi <zeyadnagi421@gmail.com>
2024-09-20 23:03:31 +0300

    css improve

---
+++ nav.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("hamburger-menu")
+  .addEventListener("click", function () {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("show");
+  });
